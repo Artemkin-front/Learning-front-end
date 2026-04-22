ff9ebe55639025e2924744f5d041e06d2a9aad5d
Артём Максимкин <maksimkin400@gmail.com>
2026-04-22 10:05:37 +0300

    Perebor

--- University/OAP/JS/Lab_3_JS/JS/3_lab_JS.js
+++ University/OAP/JS/Lab_3_JS/JS/3_lab_JS.js
@@ -1,1 +1,60 @@
- alert("sdfsf");
+let num = document.getElementById("addNumArr");
+let index = document.getElementById("delNum");
+let i = 0;
+let chetArr = [];
+let arr = document.querySelector(".arr");
+let checkch = document.querySelector(".checkChet");
+let Arr = [];
+let buf = [];
+let povtorArr = [];
+function addDiv(){
+    let addiv = document.createElement("div");
+    let numAdd = num.value;
+    Arr.push(numAdd);
+    arr.append(addiv);
+    addiv.innerText = Arr[i];
+    addiv.className = "num";
+    i++;
+    console.log(Arr);
+    checkchet();
+}
+
+function delDiv(){
+    let clas = Array.from(document.querySelectorAll(".num"));
+    let indx = index.value;
+    Arr.splice(indx,1);
+    clas[indx].remove();
+    i--;
+    checkchet();
+}
+let p = 0;
+function checkchet(){
+    let delcheck = Array.from(document.querySelectorAll(".chet"));
+    delcheck.forEach(element => {element.remove()});
+    for (let j = 0; j <= Arr.length;j++) {
+       if ((Arr[j] % 2 )== 0){
+            let addiv = document.createElement("div");
+            chetArr.push(Arr[j]);
+            console.log(j);
+            checkch.append(addiv);
+            addiv.innerText = chetArr[p];
+            addiv.className = "chet";
+            p++;
+        }
+    }
+}
+buf = Arr;
+function checkpovtor(){
+    for (let j = 0;buf.length > 0; j++) {
+        for (let s = 1;s <= buf.length; s++) {
+            if(buf[0] == buf[s]){
+                povtorArr.push(buf[s]);
+                buf.splice(s,1);
+                s--;
+            } else {
+                buf.splice(0,1);
+            }
+        }
+    }
+    console.log(buf);
+}
